8f327dbebea206bbbf2c5addac4dc9f61e1cc1d9
JAMBITI <biti.jamal@gmail.com>
2024-05-09 12:27:57 +0000

    0. Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 """module for the function island_perimeter"""
 
+
 def island_perimeter(grid):
     perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j]:
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
                 perimeter += 4
-                if i > 0 and grid[i - 1][j]:
+                if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
-                if j > 0 and grid[i][j - 1]:
+                if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
+
     return perimeter
